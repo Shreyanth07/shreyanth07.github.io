8670e14584976c0f8a6d06cba459064dc798ea43
Shreyanth S <shreyanth0810@gmail.com>
2022-12-14 10:24:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,26 +13,3 @@ valueDisplays.forEach((valueDisplay) => {
     }
   }, duration);
 });
-
-function sendMail(){
-    var params = {
-        from_name: document.getElementById("exampleFormControlInput1").value,
-        email_id: document.getElementById("exampleInputEmail1").value,
-        message: document.getElementById("exampleFormControlTextarea1").value,
-    };
-}
-
-const serviceID = "service_3cyfhlp";
-const templateID = "template_ku07uhf";
-
-emailjs
-.send(serviceID, templateID, params)
-.then((res) =>{
-        document.getElementById("exampleFormControlInput1").value = "";
-        document.getElementById("exampleInputEmail1").value = "";
-        document.getElementById("exampleFormControlTextarea1").value = "";
-        console.log(res);
-        alert("Your message is delivered to me successfully");
-    }
-)
-.catch((err) => console.log(err));
